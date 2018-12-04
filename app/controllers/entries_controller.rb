d486923b8c6132b660d48ca0c573baf634a94e4a
Benjamin <hoppeconnect@gmail.com>
2018-12-04 17:40:23 -0500

    finished part 1

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,30 +2,24 @@ class EntriesController < ApplicationController
 
 
   def index
-    render :index
-
   end
 
   def show
-    render :show
   end
 
   def new
-
   end
 
   def create
     redirect_to entries_url
-
   end
 
   def edit
     redirect_to entry_url(params[:id])
-
   end
 
-
-
-
+  def destroy
+    redirect_to entries_url
+  end
 
 end
