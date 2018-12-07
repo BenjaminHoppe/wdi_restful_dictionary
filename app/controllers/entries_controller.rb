ebaddd8970030c7cb5d5f36181d1aca68849a22e
Benjamin <hoppeconnect@gmail.com>
2018-12-07 15:51:11 -0500

    Finished assignment

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,23 +2,49 @@ class EntriesController < ApplicationController
 
 
   def index
+    @entries = Entry.all
   end
 
   def show
+    @entry = Entry.find(params[:id])
   end
 
   def new
+    @entry = Entry.new
   end
 
   def create
-    redirect_to entries_url
+    @entry = Entry.new
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+    if @entry.save
+      redirect_to "/entries"
+    else
+      render :new
+    end
   end
 
   def edit
-    redirect_to entry_url(params[:id])
+    @entry = Entry.find(params[:id])
+  end
+
+  def update
+    @entry = Entry.find(params[:id])
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
+      redirect_to entries_url
+    else
+      render :new
+    end
   end
 
   def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
     redirect_to entries_url
   end
 
